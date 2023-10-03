85c490ea455f68d68bea82645d08549257cab415
Gabor Hangyal <gaborhangyal1@gmail.com>
2023-10-03 20:03:42 +0200

    add sln

---
+++ htb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace CommandExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteCommand("dir");
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C " + command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(output);
+        }
+    }
+}
