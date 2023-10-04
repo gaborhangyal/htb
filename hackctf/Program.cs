3e16a8c288fdaf03259cddedbaea1f15fe1432d4
Gabor Hangyal <gaborhangyal1@gmail.com>
2023-10-04 20:14:30 +0200

    update

--- hackctf/Program.cs
+++ hackctf/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace CommandExecution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteCommand("dir C:\\Users\\enox");
+            ExecuteCommand("cmd.exe");
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/C " + command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(output);
+        }
+    }
+}
